6decc3131662cc8f635b691af88680d78d045cb7
yasuna <idyasuna@users.noreply.github.com>
2021-03-07 22:19:45 +0900

    210307

---
+++ js/template.js
@@ -0,0 +1,11 @@
+function footer(){
+    var html = "";
+    html += '<footer>';
+    html += '<div class="menu">'
+    html += '<ul><li><a href="qa.html">よくある質問</a></li><li><a href="attention.html">当サイトの取り扱い</a></li></ul>'
+    html += '</div>'
+    html += '<p>&copy; 2021 villains</p>';
+    html += '</footer>';
+    document.write(html);
+    }
+
