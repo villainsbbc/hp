a449019372f7f8da02e852c213d5f15ebb5174df
villainsbbc <villains.bbc@gmail.com>
2022-03-27 17:05:22 +0900

    fix to 2022

--- js/footer.js
+++ js/footer.js
@@ -4,7 +4,7 @@ function footer(){
     html += '<div class="menu">'
     html += '<ul><li><a href="qa.html">よくある質問</a></li><li><a href="attention.html">当サイトの取り扱い</a></li></ul>'
     html += '</div>'
-    html += '<p>&copy; 2021 Villains</p>';
+    html += '<p>&copy; 2022 Villains</p>';
     html += '</footer>';
     document.write(html);
     }
