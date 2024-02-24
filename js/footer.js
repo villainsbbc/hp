88a381808b651e6002d9038c8521c263dadec2b0
villainsbbc <villains.bbc@gmail.com>
2024-02-24 14:06:06 +0900

    delete twitter

--- js/footer.js
+++ js/footer.js
@@ -1,11 +1,11 @@
-function footer(){
-    var html = "";
-    html += '<footer>';
-    html += '<div class="menu">'
-    html += '<ul><li><a href="qa.html">よくある質問</a></li><li><a href="attention.html">当サイトの取り扱い</a></li></ul>'
-    html += '</div>'
-    html += '<p>&copy; 2023 Villains</p>';
-    html += '</footer>';
-    document.write(html);
-    }
-
+function footer() {
+  var html = "";
+  html += "<footer>";
+  html += '<div class="menu">';
+  html +=
+    '<ul><li><a href="qa.html">よくある質問</a></li><li><a href="attention.html">当サイトの取り扱い</a></li></ul>';
+  html += "</div>";
+  html += "<p>&copy; 2024 Villains</p>";
+  html += "</footer>";
+  document.write(html);
+}
--- js/header.js
+++ js/header.js
@@ -1,20 +1,21 @@
-function header(){
-    var html = "";
-    html += '<header>';
-    html += '<div class="row align-items-center">';
-    html += '<div class="col-2" id="col">';
-    html += '<a href="index.html"><img src="img/logo.png" class="img-logo img-fluid" ></a>';
-    html += '</div>';
-    html += '<div class="col-7" id="col">'
-    html += '<h1>女子軟式野球チーム Villains(ヴィランズ)</h1>';
-    html += '</div>';
-    html += '<div class="img-logo-sns col-3 img-fluid" id="col">';
-    html += '<a href="https://www.instagram.com/villains_bbc/" target="_blank" rel="noopener noreferrer"><img src="img/sns/insta.webp"></a>';
-    html += '<a href="https://www.facebook.com/villains89/" target="_blank" rel="noopener noreferrer"><img src="img/sns/fb.png"></a>';
-    html += '<a href="https://twitter.com/Villains_bbc" target="_blank" rel="noopener noreferrer"><img src="img/sns/twitter.png"></a>';
-    html += '</div>';
-    html += '</div>';
-    html += '</header>';
-    document.write(html);
-    }
-
+function header() {
+  var html = "";
+  html += "<header>";
+  html += '<div class="row align-items-center">';
+  html += '<div class="col-2" id="col">';
+  html +=
+    '<a href="index.html"><img src="img/logo.png" class="img-logo img-fluid" ></a>';
+  html += "</div>";
+  html += '<div class="col-7" id="col">';
+  html += "<h1>女子軟式野球チーム Villains(ヴィランズ)</h1>";
+  html += "</div>";
+  html += '<div class="img-logo-sns col-3 img-fluid" id="col">';
+  html +=
+    '<a href="https://www.instagram.com/villains_bbc/" target="_blank" rel="noopener noreferrer"><img src="img/sns/insta.webp"></a>';
+  html +=
+    '<a href="https://www.facebook.com/villains89/" target="_blank" rel="noopener noreferrer"><img src="img/sns/fb.png"></a>';
+  html += "</div>";
+  html += "</div>";
+  html += "</header>";
+  document.write(html);
+}
